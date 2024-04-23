88d768412e0e5e425622f9e6599d065183ee9697
flore-jan smeets <flore-jan_smeets@hotmail.com>
2024-04-23 13:09:00 +0200

    kleine aanpassing gedaan op components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ItemList from './Components/ItemList';
 function App() {
 
   const [items, setItems] = useState([]);
+  const [toPackItems, setToPackItems] = useState([]);
 
   const handleAddItem = (newItem) => {
     setItems([...items, newItem]);
@@ -17,28 +18,42 @@ function App() {
   };
 
   const handleUnpackItem = (index) => {
-    // Implement logic to move item back to "To Pack" list
+    const itemToUnpack = items[index];
+    const updatedItems = [...items.slice(0, index), ...items.slice(index + 1)];
+    setItems(updatedItems);
+  
+    setToPackItems([...toPackItems, itemToUnpack]);
   };
 
   return (
     <div className="App">
       <div className="max-w-[1100px] mx-auto py-8">
         <h1 className="mb-4 text-4xl font-bold">Packing list</h1>
-        <InputList onAddItem={handleAddItem} />
+        <InputList 
+            onAddItem={handleAddItem} 
+        />
         <div className="grid grid-cols-2 gap-4 mt-12 transition">
           <div>
             <header className="grid gap-2 items-center mb-6 border-b pb-6">
               <h2 className="font-bold text-2xl">To pack items</h2>
               {/* Add buttons for clearing items and filters */}
             </header>
-            <ItemList items={items} onRemoveItem={handleRemoveItem} onUnpackItem={handleUnpackItem} />
+            <ItemList 
+              items={items} 
+              onRemoveItem={handleRemoveItem} 
+              onUnpackItem={handleUnpackItem} 
+            />
           </div>
           <div>
             <header className="grid gap-2 items-center mb-6 border-b pb-6">
               <h2 className="font-bold text-2xl">Packed items</h2>
               {/* Add buttons for clearing items and filters */}
             </header>
-            <ItemList items={items} onRemoveItem={handleRemoveItem} onUnpackItem={handleUnpackItem} />
+            <ItemList 
+              items={items} 
+              onRemoveItem={handleRemoveItem} 
+              onUnpackItem={handleUnpackItem} 
+            />
           </div>
         </div>
       </div>
